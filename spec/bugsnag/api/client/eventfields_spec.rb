da77f08627ceacac8303271de07a5d11e7fa91a4
Alex Moinet <alex@bugsnag.com>
2017-09-06 16:40:48 +0100

    Ensured eventfields spec works correctly

--- spec/bugsnag/api/client/eventfields_spec.rb
+++ spec/bugsnag/api/client/eventfields_spec.rb
@@ -9,23 +9,24 @@ describe Bugsnag::Api::Client::EventFields do
 
     describe ".create_event_field" do
         it "create a new eventfield for the project" do
-            newEventfield = client.create_event_field @project_id, "event_field_test_id", "metadata/user"
+            newEventfield = @client.create_event_field @project_id, "event_field_test_id", "metadata/user", {:name => "test"}
             expect(newEventfield.display_id).to eq("event_field_test_id")
 
             assert_requested :post, bugsnag_url("/projects/#{@project_id}/event_fields")
         end
     end
 
-    context "given an event field exists"
+    context "given an event field exists", :vcr do
         before do
-            @eventfield = client.create_event_field @project_id, "event_field_test", "metadata/pivot"
+            @eventfield = @client.create_event_field @project_id, "event_field_test", "metadata/pivot", {:name => "test"}
         end
 
         describe ".list_event_fields", :vcr do
             it "lists project eventfields" do
                 eventfields = @client.list_event_fields @project_id
                 expect(eventfields).to be_kind_of(Array)
-                expect(eventfields).to include(@eventfield)
+                expect(eventfields.first.display_id).to_not be_nil
+                expect(eventfields.first.custom).to_not be_nil
 
                 assert_requested :get, bugsnag_url("/projects/#{@project_id}/event_fields")
             end
@@ -33,7 +34,7 @@ describe Bugsnag::Api::Client::EventFields do
 
         describe ".update_event_field", :vcr do
             it "updates and returns the event field" do
-                updatedEventfield = client.update_event_field @project_id, @eventfield.display_id, "metadata/test", {:pivot_options => {:name => "pivot_test"}}
+                updatedEventfield = @client.update_event_field @project_id, @eventfield.display_id, "metadata/test", {:pivot_options => {:name => "pivot_test"}}
                 expect(updatedEventfield.display_id).to eq(@eventfield.display_id)
                 expect(updatedEventfield.pivot_options.name).to eq("pivot_test")
 
@@ -43,7 +44,7 @@ describe Bugsnag::Api::Client::EventFields do
 
         describe ".delete_event_field", :vcr do
             it "deletes the event field" do
-                deletedEventfield = client.delete_event_field @project_id, @eventfield.display_id
+                deletedEventfield = @client.delete_event_field @project_id, @eventfield.display_id
                 expect(deletedEventfield).to be true
 
                 assert_requested :delete, bugsnag_url("/projects/#{@project_id}/event_fields/#{@eventfield.display_id}")
