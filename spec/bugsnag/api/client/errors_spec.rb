1cfa8843c741e3627f58599cec854c8c1b5f6059
Alex Moinet <alex@bugsnag.com>
2017-09-06 13:56:45 +0100

    Added errors cassettes (sans update_error due to issue with response)

--- spec/bugsnag/api/client/errors_spec.rb
+++ spec/bugsnag/api/client/errors_spec.rb
@@ -12,8 +12,8 @@ describe Bugsnag::Api::Client::Errors do
         it "returns errors on the project" do
             errors = @client.list_project_errors @project_id
             expect(errors).to be_a_kind_of(Array)
-            expect(errors.first.id).to not_be_nil
-            expect(errors.first.context).to not_be_nil
+            expect(errors.first.id).to_not be_nil
+            expect(errors.first.context).to_not be_nil
 
             assert_requested :get, bugsnag_url("/projects/#{@project_id}/errors")
         end
@@ -22,8 +22,8 @@ describe Bugsnag::Api::Client::Errors do
     describe ".view_error", :vcr do
         it "returns a single error" do
             error = @client.view_error @project_id, @error_id
-            expect(error.id).to not_be_nil
-            expect(error.context).to not_be_nil
+            expect(error.id).to_not be_nil
+            expect(error.context).to_not be_nil
 
             assert_requested :get, bugsnag_url("/projects/#{@project_id}/errors/#{@error_id}")
         end
@@ -32,8 +32,8 @@ describe Bugsnag::Api::Client::Errors do
     describe ".update_error", :vcr do
         it "updates and returns the updated error" do
             error = @client.update_error @project_id, @error_id, "open", {:severity => "info"}
-            expect(error.id).to not_be_nil
-            expect(error.context).to not_be_nil
+            expect(error.id).to_not be_nil
+            expect(error.context).to_not be_nil
             expect(error.severity).to eq("info")
             expect(error.status).to eq("open")
 
@@ -44,19 +44,16 @@ describe Bugsnag::Api::Client::Errors do
     describe ".bulk_update_errors", :vcr do
         it "updates and returns the updated errors" do
             errors = @client.bulk_update_errors @project_id, [@error_id], "fix", {:severity => "warn"}
-            expect(error).to be_a_kind_of(Array)
-            expect(error.first.id).to not_be_nil
-            expect(error.first.context).to not_be_nil
-            expect(error.first.severity).to eq("warn")
-            expect(error.first.status).to eq("fixed")
+            expect(errors.operation).to_not be_nil
+            expect(errors[@error_id].status).to eq("fixed")
 
-            assert_requested :patch, bugsnag_url("/projects/#{@project_id}/errors")
+            assert_requested :patch, bugsnag_url("/projects/#{@project_id}/errors?error_ids=#{@error_id}")
         end
     end
 
     describe ".delete_error", :vcr do
         it "deletes the error and returns true" do
-            stub_request(:delete, bugsnag_url("/errors/#{@error_id}").to_return(:status => [204, "No Content"])
+            stub_request(:delete, bugsnag_url("/errors/#{@error_id}")).to_return(:status => [204, "No Content"])
             
             response = @client.delete_error @error_id
             expect(response).to be true
@@ -67,7 +64,7 @@ describe Bugsnag::Api::Client::Errors do
 
     describe ".delete_all_errors", :vcr do
         it "deletes all errors and returns true" do
-            stub_request(:delete, bugsnag_url("/projects/#{@project_id}/errors").to_return(:status => [204, "No Content"])
+            stub_request(:delete, bugsnag_url("/projects/#{@project_id}/errors")).to_return(:status => [204, "No Content"])
 
             response = @client.delete_all_errors @project_id
             expect(response).to be true
