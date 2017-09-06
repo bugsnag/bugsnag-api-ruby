fe89724e19daea627314d7dffbe79d9e8c5933a7
Alex Moinet <alex@bugsnag.com>
2017-09-06 16:30:37 +0100

    Correctly formatted API call

--- lib/bugsnag/api/client/eventfields.rb
+++ lib/bugsnag/api/client/eventfields.rb
@@ -18,12 +18,11 @@ module Bugsnag
                 # Create a custom Event Field
                 #
                 # @option reindex [Boolean] Whether to reindex historical events
-                # @option filter_options [Object] Configuration for how the field will appear in the Filter Bar
                 # @option pivot_options [Object] Configuration for how this field will appear in pivots
                 # @return [Sawyer::Resource] New Event Field
                 # @see http://docs.bugsnagapiv2.apiary.io/#reference/projects/event-fields/create-a-custom-event-field
-                def create_event_field(project_id, display_id, path, options = {})
-                    post "projects/#{project_id}/event_fields", options.merge({:display_id => display_id, :path => path})
+                def create_event_field(project_id, display_id, path, filter_options, options = {})
+                    post "projects/#{project_id}/event_fields", options.merge({:display_id => display_id, :path => path, :filter_options => filter_options})
                 end
 
                 # Update a custom Event Field
