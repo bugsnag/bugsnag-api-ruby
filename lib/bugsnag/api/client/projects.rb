4c68dcccf343b5541c75a3f16beea9d4bb02f0b9
Alex Moinet <alex@bugsnag.com>
2017-10-09 11:00:30 +0100

    updated projects, whitespace

--- lib/bugsnag/api/client/projects.rb
+++ lib/bugsnag/api/client/projects.rb
@@ -1,62 +1,62 @@
 module Bugsnag
-    module Api
-        class Client
-  
-            # Methods for the Projects API
-            #
-            # @see http://docs.bugsnagapiv2.apiary.io/#reference/projects
-            module Projects
-                # Create a Project in an Organization
-                #
-                # @return [Sawyer::Resource] New Project
-                # @see http://docs.bugsnagapiv2.apiary.io/#reference/projects/projects/create-a-project-in-an-organization
-                def create_project(org_id, name, type, options = {})
-                    post "organizations/#{org_id}/projects", options.merge({:name => name, :type => type})
-                end
+  module Api
+    class Client
 
-                # View a Project
-                #
-                # @return [Sawyer::Resource] Requested Project
-                # @see http://docs.bugsnagapiv2.apiary.io/#reference/projects/projects/view-a-project
-                def view_project(id, options = {})
-                    get "projects/#{id}", options
-                end
-
-                # Update a Project
-                #
-                # @option name [String] A name for the project
-                # @option global_grouping [Array<String>] A list of error classes, which will be grouped by class
-                # @option location_grouping [Array<String>] A list of error classes, which will be grouped by context
-                # @option discarded_app_versions [Array<String>] A list of app versions whose events will be ignored
-                # @option discarded_errors [Array<String>] A list of error classes that will be ignored
-                # @option url_whitelist [Array<String>] If configured only errors from whitelisted URLs will be processed
-                # @option ignore_old_browsers [Boolean] Set to ignore events from old web browsers
-                # @option ignored_browser_versions [Object] A mapping of browser name to ignored versions
-                # @option resolve_on_deploy [Boolean] Set all errors to resolved once a new deployment has been notified
-                # @option collaborator_ids [Array<String>] Update the collaborators in the project to only these ids
-                # @return [Sawyer::Resource] Updated Project
-                # @see http://docs.bugsnagapiv2.apiary.io/#reference/projects/projects/update-a-project
-                def update_project(id, options = {})
-                    patch "projects/#{id}", options
-                end
-
-                # Regenerate a Project's notifier API key
-                #
-                # @return 
-                # @see http://docs.bugsnagapiv2.apiary.io/#reference/projects/projects/regenerate-a-project's-notifier-api-key
-                def regenerate_api_key(id, options = {})
-                    delete "projects/#{id}/api_key", options
-                end
-
-                # Delete a Project
-                #
-                # @return 
-                # @see http://docs.bugsnagapiv2.apiary.io/#reference/organizations/organizations/delete-an-organization
-                def delete_project(id, options = {})
-                    boolean_from_response :delete, "projects/#{id}", options
-                end
-            end
+      # Methods for the Projects API
+      #
+      # @see http://docs.bugsnagapiv2.apiary.io/#reference/projects
+      module Projects
+        # Create a Project in an Organization
+        #
+        # @return [Sawyer::Resource] New Project
+        # @see http://docs.bugsnagapiv2.apiary.io/#reference/projects/projects/create-a-project-in-an-organization
+        def create_project(org_id, name, type, options = {})
+          post "organizations/#{org_id}/projects", options.merge({:name => name, :type => type})
         end
+
+        # View a Project
+        #
+        # @return [Sawyer::Resource] Requested Project
+        # @see http://docs.bugsnagapiv2.apiary.io/#reference/projects/projects/view-a-project
+        def project(id, options = {})
+          get "projects/#{id}", options
+        end
+
+        # Update a Project
+        #
+        # @option name [String] A name for the project
+        # @option global_grouping [Array<String>] A list of error classes, which will be grouped by class
+        # @option location_grouping [Array<String>] A list of error classes, which will be grouped by context
+        # @option discarded_app_versions [Array<String>] A list of app versions whose events will be ignored
+        # @option discarded_errors [Array<String>] A list of error classes that will be ignored
+        # @option url_whitelist [Array<String>] If configured only errors from whitelisted URLs will be processed
+        # @option ignore_old_browsers [Boolean] Set to ignore events from old web browsers
+        # @option ignored_browser_versions [Object] A mapping of browser name to ignored versions
+        # @option resolve_on_deploy [Boolean] Set all errors to resolved once a new deployment has been notified
+        # @option collaborator_ids [Array<String>] Update the collaborators in the project to only these ids
+        # @return [Sawyer::Resource] Updated Project
+        # @see http://docs.bugsnagapiv2.apiary.io/#reference/projects/projects/update-a-project
+        def update_project(id, options = {})
+          patch "projects/#{id}", options
+        end
+
+        # Regenerate a Project's notifier API key
+        #
+        # @return 
+        # @see http://docs.bugsnagapiv2.apiary.io/#reference/projects/projects/regenerate-a-project's-notifier-api-key
+        def regenerate_api_key(id, options = {})
+          delete "projects/#{id}/api_key", options
+        end
+
+        # Delete a Project
+        #
+        # @return 
+        # @see http://docs.bugsnagapiv2.apiary.io/#reference/organizations/organizations/delete-an-organization
+        def delete_project(id, options = {})
+          boolean_from_response :delete, "projects/#{id}", options
+        end
+      end
     end
+  end
 end
   
--- spec/bugsnag/api/client/events_spec.rb
+++ spec/bugsnag/api/client/events_spec.rb
@@ -1,64 +1,64 @@
 require "spec_helper"
 
 describe Bugsnag::Api::Client::Events do
-    before do
-        @client = auth_token_client
-        @project_id = test_bugsnag_project_id
-        @error_id = test_bugsnag_error_id
-        @event_id = test_bugsnag_event_id
-        Bugsnag::Api.reset!
+  before do
+    @client = auth_token_client
+    @project_id = test_bugsnag_project_id
+    @error_id = test_bugsnag_error_id
+    @event_id = test_bugsnag_event_id
+    Bugsnag::Api.reset!
+  end
+
+  describe ".view_event", :vcr do
+    it "returns the specified event" do
+      event = @client.view_event @project_id, @event_id
+      expect(event.id).to_not be_nil
+      expect(event.context).to_not be_nil
+
+      assert_requested :get, bugsnag_url("/projects/#{@project_id}/events/#{@event_id}")
     end
+  end
 
-    describe ".view_event", :vcr do
-        it "returns the specified event" do
-            event = @client.view_event @project_id, @event_id
-            expect(event.id).to_not be_nil
-            expect(event.context).to_not be_nil
+  describe ".delete_event", :vcr do
+    it "deletes the specified event" do
+      stub_request(:delete, bugsnag_url("/projects/#{@project_id}/events/#{@event_id}")).to_return(:status => [204, "No Content"])
 
-            assert_requested :get, bugsnag_url("/projects/#{@project_id}/events/#{@event_id}")
-        end
+      response = @client.delete_event @project_id, @event_id
+      expect(response).to be true
+
+      assert_requested :delete, bugsnag_url("/projects/#{@project_id}/events/#{@event_id}")
     end
+  end
 
-    describe ".delete_event", :vcr do
-        it "deletes the specified event" do
-            stub_request(:delete, bugsnag_url("/projects/#{@project_id}/events/#{@event_id}")).to_return(:status => [204, "No Content"])
-
-            response = @client.delete_event @project_id, @event_id
-            expect(response).to be true
-
-            assert_requested :delete, bugsnag_url("/projects/#{@project_id}/events/#{@event_id}")
-        end
+  describe ".list_error_events", :vcr do
+    it "lists all error events" do
+      events = @client.list_error_events @project_id, @error_id
+      expect(events).to be_a_kind_of(Array)
+      expect(events.first.id).to_not be_nil
+      expect(events.first.context).to_not be_nil
+      
+      assert_requested :get, bugsnag_url("/projects/#{@project_id}/errors/#{@error_id}/events")
     end
+  end
 
-    describe ".list_error_events", :vcr do
-        it "lists all error events" do
-            events = @client.list_error_events @project_id, @error_id
-            expect(events).to be_a_kind_of(Array)
-            expect(events.first.id).to_not be_nil
-            expect(events.first.context).to_not be_nil
-            
-            assert_requested :get, bugsnag_url("/projects/#{@project_id}/errors/#{@error_id}/events")
-        end
+  describe ".view_latest_event", :vcr do
+    it "returns the last event on an error" do
+      event = @client.view_latest_event @error_id
+      expect(event.id).to_not be_nil
+      expect(event.context).to_not be_nil
+
+      assert_requested :get, bugsnag_url("/errors/#{@error_id}/latest_event")
     end
+  end
 
-    describe ".view_latest_event", :vcr do
-        it "returns the last event on an error" do
-            event = @client.view_latest_event @error_id
-            expect(event.id).to_not be_nil
-            expect(event.context).to_not be_nil
+  describe ".list_project_events", :vcr do
+    it "returns the a list of project errors" do
+      events = @client.list_project_events @project_id
+      expect(events).to be_a_kind_of(Array)
+      expect(events.first.id).to_not be_nil
+      expect(events.first.context).to_not be_nil
 
-            assert_requested :get, bugsnag_url("/errors/#{@error_id}/latest_event")
-        end
+      assert_requested :get, bugsnag_url("/projects/#{@project_id}/events")
     end
-
-    describe ".list_project_events", :vcr do
-        it "returns the a list of project errors" do
-            events = @client.list_project_events @project_id
-            expect(events).to be_a_kind_of(Array)
-            expect(events.first.id).to_not be_nil
-            expect(events.first.context).to_not be_nil
-
-            assert_requested :get, bugsnag_url("/projects/#{@project_id}/events")
-        end
-    end
+  end
 end
