9de0834c9934e61700ff7ca5f9c65327be5c2432
Alex Moinet <alex@bugsnag.com>
2017-10-09 10:31:30 +0100

    ADded comments API & spec

---
+++ lib/bugsnag/api/client/comments.rb
@@ -0,0 +1,55 @@
+module Bugsnag
+  module Api
+    class Client
+
+      # Methods for the Errors API
+      #
+      # @see http://docs.bugsnagapiv2.apiary.io/#reference/comments
+      module Comments
+        # List Comments on an Error
+        #
+        # @option offset [Number] The pagination offset (not required for general use)
+        # @option sort [String] Comments are only sortable by creation time e.g. "created_at"
+        # @option direction [String] Which direction to sort the result by, one of: asc, desc
+        # @option per_page [Number] How many results to return per page
+        # @return [Array<Sawyer::Resource>] List of Error Comments
+        # @see http://docs.bugsnagapiv2.apiary.io/#reference/errors/comments/list-comments-on-an-error
+        def comments(project_id, error_id, options = {})
+            paginate "projects/#{project_id}/errors/#{error_id}/comments", options
+        end
+
+        # View an Comment
+        #
+        # @return [Sawyer::Resource] Requested Comment
+        # @see http://docs.bugsnagapiv2.apiary.io/#reference/errors/comments/view-a-comment
+        def comment(id, options = {})
+            get "comments/#{id}", options
+        end
+
+        # Create a Comment on an Error
+        #
+        # @return [Sawyer::Resource] The new Comment
+        # @see http://docs.bugsnagapiv2.apiary.io/#reference/errors/comments/create-a-comment-on-an-error
+        def create_comment(project_id, error_id, message, options = {})
+            post "projects/#{project_id}/errors/#{error_id}/comments", options.merge({:message => message})
+        end
+
+        # Update a Comment
+        #
+        # @return [Sawyer::Resource] The updated Comment
+        # @see http://docs.bugsnagapiv2.apiary.io/#reference/errors/comments/update-a-comment
+        def update_comment(id, message, options = {})
+            patch "comments/#{id}", options
+        end
+
+        # Delete a Comment
+        #
+        # @return 
+        # @see http://docs.bugsnagapiv2.apiary.io/#reference/errors/comments/delete-a-comment
+        def delete_comment(id, options = {})
+            boolean_from_response :delete, "comments/#{id}", options
+        end
+      end
+    end
+  end
+end
---
+++ spec/bugsnag/api/client/comments_spec.rb
@@ -0,0 +1,72 @@
+require "spec_helper"
+
+describe Bugsnag::Api::Client::Comments do
+  before do
+    @client = auth_token_client
+    @project_id = test_bugsnag_project_id
+    @error_id = test_bugsnag_error_id
+    Bugsnag::Api.reset!
+  end
+
+  describe ".create_comment", :vcr do
+    it "creates a comment on the error" do
+      client = auth_token_client
+      comment = client.create_comment @project_id, @error_id, "test_message"
+      expect(comment.message).to eq("test_message")
+      expect(comment.id).to_not be_nil
+
+      assert_requested :post, bugsnag_url("/projects/#{@project_id}/errors/#{@error_id}/comments")
+    end
+  end
+
+  context "given a comment has been created" do
+    before do
+      @comment = client.create_comment @project_id, @error_id, "message"
+    end
+
+
+    describe ".comments", :vcr do
+      it "retrieves all comments on an error" do
+        comments = @client.comments @project_id, @error_id
+
+        expect(comments).to be_kind_of(Array)
+        expect(comments.first.message).to_not be_nil
+
+        assert_requested :get, bugsnag_url("/projects/#{@project_id}/errors/#{error_id}/comments")
+      end
+    end
+
+    describe ".comment", :vcr do
+      it "retrieves the comment specified" do
+        comment = @client.comment @comment.id
+
+        expect(comment.message).to eq("message")
+        expect(comment.id).to eq(@comment.id)
+
+        assert_requested :get, bugsnag_url("/comments/#{@comment.id}")
+      end
+    end
+
+    describe ".update_comment", :vcr do
+      it "updates the message on a comment" do
+        comment = @client.update_comment @comment.id, "updated"
+        
+        expect(comment.message).to eq("updated")
+        expect(comment.id).to eq(@comment.id)
+
+        assert_requested :patch, bugsnag_url("/comments/#{@comment.id}")
+      end
+    end
+
+    describe ".delete_comment", :vcr do
+      it "deletes the comment and returns true" do
+        stub_request(:delete, bugsnag_url("/comments/#{@comment.id}")).to_return(:status => [204, "No Content"])
+        
+        response = @client.delete_error @comment.id
+        expect(response).to be true
+
+        assert_requested :delete, bugsnag_url("/comments/#{@comment.id}")
+      end
+    end
+  end
+end
