56a48a3dd0ba9e30b47abeb3afe190cbdf167812
Alex Moinet <alex@bugsnag.com>
2017-10-09 14:10:39 +0100

    minor updates and name changes

--- lib/bugsnag/api/client/collaborators.rb
+++ lib/bugsnag/api/client/collaborators.rb
@@ -11,7 +11,7 @@ module Bugsnag
         # @return [Sawyer::Resource] Collaborator
         # @see http://docs.bugsnagapiv2.apiary.io/#reference/organizations/collaborators/view-a-collaborator
         def collaborator(org_id, collaborator_id, options = {})
-            get "organizations/#{org_id}/collaborators/#{collaborator_id}", options
+          get "organizations/#{org_id}/collaborators/#{collaborator_id}", options
         end
 
         # List Collaborators
@@ -24,13 +24,13 @@ module Bugsnag
         # @see http://docs.bugsnagapiv2.apiary.io/#reference/organizations/collaborators/list-collaborators
         # @see http://docs.bugsnagapiv2.apiary.io/#reference/organizations/collaborators/list-collaborators-on-a-project
         def collaborators(org_id = nil, project_id = nil, options = {})
-            if !org_id.nil?
-                paginate "organizations/#{org_id}/collaborators", options
-            elsif !project_id.nil?
-                paginate "projects/#{project_id}/collaborators", options
-            else
-                raise ArgumentError, "Must supply either an org_id or a project_id"
-            end
+          if !org_id.nil?
+            paginate "organizations/#{org_id}/collaborators", options
+          elsif !project_id.nil?
+            paginate "projects/#{project_id}/collaborators", options
+          else
+            raise ArgumentError, "Must supply either an org_id or a project_id"
+          end
         end
 
         # Invite Collaborators
@@ -42,14 +42,14 @@ module Bugsnag
         # @return [Array<Sawyer::Resource>] Collaborator details
         # @see http://docs.bugsnagapiv2.apiary.io/#reference/organizations/collaborators/bulk-invite-collaborators
         def invite_collaborators(org_id, emails, options = {})
-            case emails
-            when String
-              post "organizations/#{org_id}/collaborators", options.merge({:email => emails})
-            when Array
-              post "organizations/#{org_id}/collaborators/bulk_invite", options.merge({:emails => emails})
-            else
-              raise ArgumentError, "Emails must be a String or an Array"
-            end
+          case emails
+          when String
+            post "organizations/#{org_id}/collaborators", options.merge({:email => emails})
+          when Array
+            post "organizations/#{org_id}/collaborators/bulk_invite", options.merge({:emails => emails})
+          else
+            raise ArgumentError, "Emails must be a String or an Array"
+          end
         end
 
         # Update a Collaborator's permissions
@@ -59,7 +59,7 @@ module Bugsnag
         # @return [Sawyer::Resource] Collaborator details
         # @see http://docs.bugsnagapiv2.apiary.io/#reference/organizations/collaborators/update-a-collaborator's-permissions
         def update_collaborator_permissions(org_id, id, options = {})
-            patch "organizations/#{org_id}/collaborators/#{id}", options
+          patch "organizations/#{org_id}/collaborators/#{id}", options
         end
 
         # Delete a Collaborator
@@ -67,7 +67,7 @@ module Bugsnag
         # @return
         # @see http://docs.bugsnagapiv2.apiary.io/#reference/organizations/collaborators/delete-a-collaborator
         def delete_collaborator(org_id, id, options = {})
-            boolean_from_response :delete, "organizations/#{org_id}/collaborators/#{id}", options
+          boolean_from_response :delete, "organizations/#{org_id}/collaborators/#{id}", options
         end
 
         # View Projects a Collaborator has access to
@@ -78,7 +78,7 @@ module Bugsnag
         # @return [Array<Sawyer::Resource>] A list of projects the collaborator belongs to
         # @see http://docs.bugsnagapiv2.apiary.io/#reference/organizations/collaborators/view-projects-a-collaborator-has-access-to
         def view_collaborator_projects(org_id, id, options = {})
-            paginate "organizations/#{org_id}/collaborators/#{id}/projects", options
+          paginate "organizations/#{org_id}/collaborators/#{id}/projects", options
         end
       end
     end
--- lib/bugsnag/api/client/comments.rb
+++ lib/bugsnag/api/client/comments.rb
@@ -15,7 +15,7 @@ module Bugsnag
         # @return [Array<Sawyer::Resource>] List of Error Comments
         # @see http://docs.bugsnagapiv2.apiary.io/#reference/errors/comments/list-comments-on-an-error
         def comments(project_id, error_id, options = {})
-            paginate "projects/#{project_id}/errors/#{error_id}/comments", options
+          paginate "projects/#{project_id}/errors/#{error_id}/comments", options
         end
 
         # View an Comment
@@ -23,7 +23,7 @@ module Bugsnag
         # @return [Sawyer::Resource] Requested Comment
         # @see http://docs.bugsnagapiv2.apiary.io/#reference/errors/comments/view-a-comment
         def comment(id, options = {})
-            get "comments/#{id}", options
+          get "comments/#{id}", options
         end
 
         # Create a Comment on an Error
@@ -31,7 +31,7 @@ module Bugsnag
         # @return [Sawyer::Resource] The new Comment
         # @see http://docs.bugsnagapiv2.apiary.io/#reference/errors/comments/create-a-comment-on-an-error
         def create_comment(project_id, error_id, message, options = {})
-            post "projects/#{project_id}/errors/#{error_id}/comments", options.merge({:message => message})
+          post "projects/#{project_id}/errors/#{error_id}/comments", options.merge({:message => message})
         end
 
         # Update a Comment
@@ -39,7 +39,7 @@ module Bugsnag
         # @return [Sawyer::Resource] The updated Comment
         # @see http://docs.bugsnagapiv2.apiary.io/#reference/errors/comments/update-a-comment
         def update_comment(id, message, options = {})
-            patch "comments/#{id}", options.merge({:message => message})
+          patch "comments/#{id}", options.merge({:message => message})
         end
 
         # Delete a Comment
@@ -47,7 +47,7 @@ module Bugsnag
         # @return 
         # @see http://docs.bugsnagapiv2.apiary.io/#reference/errors/comments/delete-a-comment
         def delete_comment(id, options = {})
-            boolean_from_response :delete, "comments/#{id}", options
+          boolean_from_response :delete, "comments/#{id}", options
         end
       end
     end
